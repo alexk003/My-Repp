a0227dfc7a0d7ff5e585be27868fac349159114e
Алексей Комаров <alexk003@yandex.ru>
2023-04-30 11:32:00 +0300

    Оптимизировал ф-ю handler, убрал повторения, улучшил работу с БД

--- inteface.py
+++ inteface.py
@@ -24,38 +24,46 @@ class BotInterface:
                         )
 
     def handler(self):
+        offset = 0
         longpull = VkLongPoll(self.bot)
         for event in longpull.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                 data_store.create_table(conn)
-                data_store.clear_table(conn)
                 prof_info = core.tools.get_profile_info(event.user_id)
                 city_id = prof_info[0].get('city').get('id')
                 birthday = prof_info[0].get('bdate')
                 birth_year = birthday.split('.')[2]
                 age_from = current_year - int(birth_year) - 5
-                age_to = current_year - int(birth_year) + 5
+                age_to = current_year - int(birth_year)
                 sex = prof_info[0].get('sex')
-                if sex == 1:
-                    sex = 2
-                    self.message_send(event.user_id, 'Вы женщина, ищем мужчину, возраст: +- 5 лет :-)')
-                elif sex == 2:
-                    self.message_send(event.user_id, 'Вы мужчина, ищем женщину, возраст: +- 5 лет :-)')
-                    sex = 1
-                else:
-                    sex = int(input('ошибка определения пола, введите Ваш пол:'
-                                    ' 1 - если Вы женщина, 2 - если Вы мужчина'))
                 if event.text.lower() == 'привет':
                     self.message_send(event.user_id, 'Добрый день, напишите команду "поиск" чтобы увидеть результат!')
-                elif event.text.lower() == 'поиск':
-                    result_prof = core.tools.user_serch(city_id, age_from, age_to, sex, 6,)
+                elif event.text.lower() == 'поиск' or event.text.lower() == 'далее':
+                    if sex == 1:
+                        sex = 2
+                        self.message_send(event.user_id, 'Вы женщина, ищем мужчину, возраст: +- 5 лет :-)')
+                    elif sex == 2:
+                        self.message_send(event.user_id, 'Вы мужчина, ищем женщину, возраст: +- 5 лет :-)')
+                        sex = 1
+                    else:
+                        sex = int(input('ошибка определения пола, введите Ваш пол:'
+                                        ' 1 - если Вы женщина, 2 - если Вы мужчина'))
+                    result_prof = core.tools.user_serch(city_id, age_from, age_to, sex, 6, offset)
                     for profile in result_prof:
+                        offset += 1
                         id_found = profile.get('id')
                         id_list = data_store.from_db(conn, event.user_id)
-                        if id_found in id_list:
+                        print(id_list)
+                        print(id_found)
+                        print(id_list[0][0])
+                        list_id =[]
+                        for x in id_list:
+                            list_id.append(x[0])
+                        print(list_id)
+                        if id_found in list_id:
                             continue
                         else:
-                            data_store.to_bd(conn, event.user_id, id_found)
+                            data_store.to_db(conn, event.user_id, id_found)
                             self.message_send(event.user_id, 'https://vk.com/id' + str(id_found))
                             result_photos_get = core.tools.photos_get(id_found)
                             for photo in result_photos_get:
@@ -63,28 +71,7 @@ class BotInterface:
                                 owner_id = photo.get('owner_id')
                                 media = 'photo' + str(owner_id) + '_' + str(photo_id)
                                 self.message_send(event.user_id, attachment= media)
-                    self.message_send(event.user_id, 'Напишите "далее", если хотите продолжить поиск!')
-                elif event.text.lower() == 'далее':
-                    if offset >= 1000:
-                        self.message_send(event.user_id, 'Вы исчерпали лимит в 1000 запросов!')
-                    else:
-                        self.prof_info(event.user_id)
-                        offset += 25
-                        result_prof = core.tools.user_serch(bot.city_id, bot.age_from, bot.age_to, bot.sex, 6, offset)
-                        for profile in result_prof:
-                            id_found = profile.get('id')
-                            id_list = data_store.from_db(conn, event.user_id)
-                            if id_found in id_list:
-                                continue
-                            else:
-                                data_store.to_bd(conn, event.user_id, id_found)
-                                self.message_send(event.user_id, 'https://vk.com/id' + str(id_found))
-                                result_photos_get = core.tools.photos_get(id_found)
-                                for photo in result_photos_get:
-                                    photo_id = photo.get('id')
-                                    owner_id = photo.get('owner_id')
-                                    media = 'photo' + str(owner_id) + '_' + str(photo_id)
-                                    self.message_send(event.user_id, attachment= media)
+                    self.message_send(event.user_id, 'Напишите "поиск" или "далее", если хотите продолжить поиск!')
                 else:
                     self.message_send(event.user_id, 'извините, пока я знаю только "привет", "поиск", '
                                                      '"далее" и "пока" :-)')
